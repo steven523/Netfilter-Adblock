70fd0022d85fb09fcf257ca55bcc080941b764a3
steven523 <asdzxc6pe613@gmail.com>
2024-06-28 14:36:19 +0800

    Added hosts file content and tests

--- kadblock.c
+++ kadblock.c
@@ -35,14 +35,20 @@ static int extract_tcp_data(struct sk_buff *skb, char **data)
     data_off = ip->ihl * 4 + tcp->doff * 4;
     data_len = skb->len - data_off;
 
-    if (data_len == 0)
+    if (data_len == 0){
+        // printk("AAAAAAAAAAA");
         return -1;
-
+    }
     if (skb_linearize(skb))
         return -1;
 
     *data = skb->data + data_off;
-
+    // if (ntohs(udp_hdr(skb)->dest) == 53 || ntohs(udp_hdr(skb)->dest) == 80){
+    //     printk("Get TCP bbbbbbbbbbbbbbbbbbbbbbb23336852");
+    // }
+    // if (ntohs(udp_hdr(skb)->dest) == 443){
+    //     printk("Get TLS 443");
+    // }
     return data_len;
 }
 
@@ -76,7 +82,7 @@ static int extract_udp_data(struct sk_buff *skb, char **data)
     return data_len;
 }
 
-static unsigned int blocker_hook(void *priv,
+static unsigned int filter_hook(void *priv,
                                  struct sk_buff *skb,
                                  const struct nf_hook_state *state)
 {
@@ -96,13 +102,23 @@ static unsigned int blocker_hook(void *priv,
         tcpflag = 1;
         if (data[0] == 0x16) {
             printk("TLS handshake len: %d", len);
-            printk("bbbbbb");
             proto = tls_protocol;
         } else if (strncmp(data, "GET ", sizeof("GET ") - 1) == 0) {
             proto = http_protocol;
+            printk("HTTP GET request detected.");
         }
     }
-    
+    // if (len != -1) {
+    //     tcpflag = 1;
+    //     if (data[0] == 0x16) {
+    //         printk("TLS handshake len: %d", len);
+    //         proto = tls_protocol;
+    //     } else if (strncmp(data, "GET ", 4) == 0) {
+    //         proto = http_protocol;
+    //         printk("HTTP GET request detected.");
+    //     }
+    // }
+
     /*
         Extract UDP data
     */
@@ -110,6 +126,7 @@ static unsigned int blocker_hook(void *priv,
     if (len != -1) {
         if (ntohs(udp_hdr(skb)->dest) == 53) {
             proto = dns_protocol;
+            printk("Get UDP");
         }
     }
 
@@ -132,6 +149,7 @@ static unsigned int blocker_hook(void *priv,
                 send_server_ack(skb, state);
                 send_close(skb, proto, state);
                 send_tcp_reset(skb, state);
+                printk("send tcp control");
             }
         }
         kfree(host);
@@ -139,18 +157,18 @@ static unsigned int blocker_hook(void *priv,
 
     return ret;
 }
-static struct nf_hook_ops blocker_ops = {.hook = blocker_hook,
+static struct nf_hook_ops filter_ops = {.hook = filter_hook,
                                          .pf = NFPROTO_IPV4,
-                                         .hooknum = NF_INET_LOCAL_OUT};
+                                         .hooknum = NF_INET_PRE_ROUTING};
 
-static int mod_init(void)
+static int __init mod_init(void)
 {
-    return nf_register_net_hook(&init_net, &blocker_ops);
+    return nf_register_net_hook(&init_net, &filter_ops);
 }
 
-static void mod_exit(void)
+static void __exit mod_exit(void)
 {
-    nf_unregister_net_hook(&init_net, &blocker_ops);
+    nf_unregister_net_hook(&init_net, &filter_ops);
 }
 
 module_init(mod_init);
